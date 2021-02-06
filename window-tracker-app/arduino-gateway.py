0af3d52ee8cf6f49099161659bc85b4c6a33b63c
Rifad Lafir <rifadahmedlafir@gmail.com>
2021-02-06 23:57:57 +0100

    add taunts

--- window-tracker-app/arduino-gateway.py
+++ window-tracker-app/arduino-gateway.py
@@ -7,6 +7,8 @@ from datetime import datetime
 from threading import Thread, Lock
 from audio import play_audio
 
+TIME_INTERVAL = 5
+
 # Globally accessible
 ser = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=5)
 
@@ -14,24 +16,32 @@ serial_lock = Lock()
 
 def eventMonitoring():
     time_spent_procrastinating = 0
+    prompts = 0
 
     bad_apps = set(["facebook", "twitter", "netflix", "reddit", 
                     "quora", "messenger", "9gag", "instagram", "linkedin", "whatsapp"])
 
     # Receive events from Shell Script
     for line in sys.stdin: 
-        event_string = re.sub("[^A-Za-z0-9]+", ' ', line.strip().lower()).split(" ")
+        event_string = set(re.sub("[^A-Za-z0-9]+", ' ', line.strip().lower()).split(" "))
   
-        for keyword in event_string:
-            if keyword in bad_apps:
-                print ("Bad app - {}".format(keyword))
-                serial_lock.acquire()
-                ser.write(bytes("a","UTF-8"))
-                serial_lock.release()
-                
-                time_spent_procrastinating += 1
-                if time_spent_procrastinating % 5 == 0:
-                    Thread(target=play_audio, args=(keyword, )).start()
+        bad_app = list(event_string.intersection(bad_apps))
+        is_bad_app = len(bad_app) != 0
+        if is_bad_app:
+            time_spent_procrastinating += 1
+            print ("Bad app - {} - time spent: {} - prompted: {}".format(bad_app[0], time_spent_procrastinating, prompts))
+            
+            serial_lock.acquire()
+            ser.write(bytes("a","UTF-8"))
+            serial_lock.release()
+            
+            if time_spent_procrastinating % TIME_INTERVAL == 0:
+                prompts += 1
+                thread = Thread(target=play_audio, args=(prompts, bad_app)).start()
+        else:
+            prompts = 0
+            time_spent_procrastinating = 0
+
 
             
 if __name__ == "__main__":
--- window-tracker-app/audio.py
+++ window-tracker-app/audio.py
@@ -1,19 +1,68 @@
 from google.cloud import texttospeech
 from playsound import playsound
+import time 
+import html
 
 # run export GOOGLE_APPLICATION_CREDENTIALS="impractical-hackers-ce3c2d8b1bea.json"
+
+taunts = {
+    1: "Stop browsing {}.",
+    2: "Get back to work. Browse {} later.",
+    3: "Ok honestly, you need to get back to work. {} will not pay the bills.",
+    4: "Seriously, you are getting on my nerves. You need to stop this right now.",
+    5: "Your machine will self destruct in 5. 4. 3. 2. 1.",
+    6: "Wow. {} is more important than your own life. I see how it is.",
+    7: "I give up. You clearly do not care."
+}
+
+MAX_PROMPTS = len(taunts)
+
+def text_to_ssml(raw_lines):
+    # Generates SSML text from plaintext.
+    # Given an input filename, this function converts the contents of the text
+    # file into a string of formatted SSML text. This function formats the SSML
+    # string so that, when synthesized, the synthetic audio will pause for two
+    # seconds between each line of the text file. This function also handles
+    # special text characters which might interfere with SSML commands.
+    #
+    # Args:
+    # inputfile: string name of plaintext file
+    #
+    # Returns:
+    # A string of SSML text based on plaintext input
+
+    # Replace special characters with HTML Ampersand Character Codes
+    # These Codes prevent the API from confusing text with
+    # SSML commands
+    # For example, '<' --> '&lt;' and '&' --> '&amp;'
+
+    escaped_lines = html.escape(raw_lines)
+
+    # Convert plaintext to SSML
+    # Wait two seconds between each address
+    ssml = "<speak>{}</speak>".format(
+        escaped_lines.replace(".", '<break time="0.5s"/>')
+    )
+
+    # Return the concatenated string of ssml script
+    return ssml
 
 # Instantiates a client
 client = texttospeech.TextToSpeechClient()
 
-def play_audio(app="facebook"):
+def play_audio(prompts, app="facebook"):
+
+  if prompts > MAX_PROMPTS:
+    return
+
   # Set the text input to be synthesized
-  synthesis_input = texttospeech.SynthesisInput(text="Stop browsing {}".format(app))
+  text_to_say = text_to_ssml(taunts.get(prompts).format(app))
+  synthesis_input = texttospeech.SynthesisInput(ssml=text_to_say)
 
   # Build the voice request, select the language code ("en-US") and the ssml
   # voice gender ("neutral")
   voice = texttospeech.VoiceSelectionParams(
-      language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+      language_code="en-UK", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
   )
 
   # Select the type of audio file you want returned
@@ -35,4 +84,6 @@ def play_audio(app="facebook"):
   playsound("output.mp3")
 
 if __name__ == "__main__":
-  play_audio()
+  for i in range(1, 8):
+      play_audio(i, "netflix")
+      time.sleep(1)
